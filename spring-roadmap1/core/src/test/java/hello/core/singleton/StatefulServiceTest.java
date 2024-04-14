638d1d6b73c1066b370766f586ccb956491b6acb
Tycoon19 <kevin960831@naver.com>
2024-04-14 10:05:32 +0900

    feat: @Configuration 을 통한 싱글톤 관리 테스트

--- spring-roadmap1/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ spring-roadmap1/core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,13 +1,10 @@
 package hello.core.singleton;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class StatefulServiceTest {
     @Test
